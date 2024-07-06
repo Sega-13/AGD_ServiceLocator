35d7715a099d7bb3af073d3ca04d33ad645ade1a
Sega-13 <segapatidar@gmail.com>
2024-07-06 18:01:36 +0530

    Implemented Service locater for Sound & UI services.

--- Assets/Scripts/main/GameService.cs
+++ Assets/Scripts/main/GameService.cs
@@ -1,4 +1,6 @@
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,10 +9,19 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService {  get; private set; }
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    public SoundService soundService { get; private set; }
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private UIService uIService;
+    public UIService UIService => uIService;
+
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
     void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject,audioEffects,backgroundMusic);
+        
     }
 
     void Update()
